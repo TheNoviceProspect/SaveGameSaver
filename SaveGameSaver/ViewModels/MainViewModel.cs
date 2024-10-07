490adc7bec860ba7273c2feb65fdfab454eb8470
Sebastian Meier zu Biesen <mitoskalandiel@gmail.com>
2024-10-07 11:07:52 +0100

    feat: Add Backup and Restore UI elements
    Add UI elements for Backup and Restore functionality
    Enable the UI elements to interact with the backend
    Improve the user experience by adding more visual feedback
    (CI: removed restore step and restore DURING build)

--- SaveGameSaver/ViewModels/MainViewModel.cs
+++ SaveGameSaver/ViewModels/MainViewModel.cs
@@ -1,6 +1,25 @@
-﻿namespace SaveGameSaver.Core.ViewModels;
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace SaveGameSaver.Core.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
     public string Greeting => "Welcome to Avalonia!";
+    public ICommand BackupCommand { get; }
+    public ICommand RestoreCommand { get; }
+
+    public MainViewModel()
+    {
+        BackupCommand = new RelayCommand(Backup);
+        RestoreCommand = new RelayCommand(Restore);
+    }
+
+    private void Backup()
+    {
+    }
+
+    private void Restore()
+    {
+    }
 }
